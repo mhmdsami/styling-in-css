b3e6304ba73a1a86308fc573a9b5ca8fd8aa4cb4
Mohamed Sami <srmf2025@gmail.com>
2023-04-29 20:14:23 +0530

    :sparkles: complete inline-styles

--- inline-styles/src/App.tsx
+++ inline-styles/src/App.tsx
@@ -2,15 +2,40 @@ import posts from './posts'
 
 function App() {
   return (
-    <div>
+    <div style={{
+      margin: '48px',
+      display: 'grid',
+      gap: '24px',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}>
       {posts.map(post => (
-        <div>
-          <div>
-            <div>{post.title}</div>
+        <div style={{
+          backgroundColor: '#DFE0DC',
+          padding: '20px',
+          width: '30vw',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '16px',
+          borderRadius: '8px',
+        }}>
+          <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '2px',
+            fontSize: '14px',
+          }}>
+            <div style={{ fontSize: '20px', fontWeight: '700' }}>{post.title}</div>
             <div>{post.author}, {post.date}</div>
           </div>
-          <img alt={post.title} src={post.image} />
-          <div>{post.content}</div>
+          <img style={{
+            width: '100%',
+            height: '200px',
+            objectFit: 'cover',
+            borderRadius: '8px',
+          }} alt={post.title} src={post.image} />
+          <div style={{ fontSize: '16px' }}>{post.content}</div>
         </div>
       ))}
     </div>
