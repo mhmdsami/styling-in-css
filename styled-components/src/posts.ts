5fae288283b4e50e135a319fd08b300ef4fd5f5d
Mohamed Sami <srmf2025@gmail.com>
2023-04-29 18:36:00 +0530

    :tada: initial commit

---
+++ styled-components/src/posts.ts
@@ -0,0 +1,47 @@
+export default [
+  {
+    id: 1,
+    title: "The Future of JavaScript: A Look Ahead",
+    author: "John Doe",
+    content: "JavaScript has become the most widely used programming language in the world. In this post, we take a look at what the future holds for JavaScript and what new features we can expect to see in the years to come.",
+    image: "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1120&q=80",
+    date: "2022-04-04",
+    likes: 35
+  },
+  {
+    id: 2,
+    title: "How to Build a Responsive Website with CSS Grid",
+    author: "Jane Smith",
+    content: "CSS Grid is a powerful tool that allows you to create complex layouts with ease. In this post, we'll show you how to use CSS Grid to build a responsive website that looks great on any device.",
+    image: "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1120&q=80",
+    date: "2022-04-11",
+    likes: 42
+  },
+  {
+    id: 3,
+    title: "Building Modern Web Applications with React",
+    author: "Alex Johnson",
+    content: "React has quickly become the go-to framework for building modern web applications. In this post, we'll show you how to get started with React and build a simple application that demonstrates some of its key features.",
+    image: "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1120&q=80",
+    date: "2022-04-18",
+    likes: 27
+  },
+  {
+    id: 4,
+    title: "The Benefits of Serverless Architecture for Web Applications",
+    author: "Sarah Lee",
+    content: "Serverless architecture is a new paradigm that allows developers to build and run applications without worrying about server infrastructure. In this post, we'll explore the benefits of serverless architecture for web applications.",
+    image: "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1120&q=80",
+    date: "2022-04-25",
+    likes: 13
+  },
+  {
+    id: 5,
+    title: "Building Scalable Web Applications with Node.js",
+    author: "Mike Brown",
+    content: "Node.js has revolutionized the way we build web applications, allowing us to build scalable, high-performance applications with ease. In this post, we'll show you how to use Node.js to build a simple web application that can handle high traffic loads.",
+    image: "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1120&q=80",
+    date: "2022-05-01",
+    likes: 18
+  }
+];
---
+++ tailwind/src/App.tsx
@@ -0,0 +1,20 @@
+import posts from './posts'
+
+function App() {
+  return (
+    <div>
+      {posts.map(post => (
+        <div>
+          <div>
+            <div>{post.title}</div>
+            <div>{post.author}, {post.date}</div>
+          </div>
+          <img alt={post.title} src={post.image} />
+          <div>{post.content}</div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default App;
