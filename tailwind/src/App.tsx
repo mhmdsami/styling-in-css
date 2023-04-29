0f60455db1e536f629aeef265cba8c6237cc2f20
Mohamed Sami <srmf2025@gmail.com>
2023-04-29 20:15:33 +0530

    :sparkles: complete tailwind

--- tailwind/src/App.tsx
+++ tailwind/src/App.tsx
@@ -2,15 +2,15 @@ import posts from './posts'
 
 function App() {
   return (
-    <div>
+    <div className="m-12 gap-6 flex flex-col justify-center items-center">
       {posts.map(post => (
-        <div>
-          <div>
-            <div>{post.title}</div>
+        <div className="flex flex-col bg-[#DFE0DC] p-5 w-[30vw] gap-4 rounded-lg">
+          <div className="flex flex-col gap-0.5 text-sm">
+            <div className="text-xl font-bold">{post.title}</div>
             <div>{post.author}, {post.date}</div>
           </div>
-          <img alt={post.title} src={post.image} />
-          <div>{post.content}</div>
+          <img className="w-full h-[200px] object-cover rounded-lg" alt={post.title} src={post.image} />
+          <div className="text-base">{post.content}</div>
         </div>
       ))}
     </div>
